c87464a870f092b5b7f0b9a752ab4f945a8b0b4a
jiro4989 <jiroron666@gmail.com>
2019-08-02 21:49:55 +0900

    :white_check_mark: Add example codes

--- examples_test.go
+++ examples_test.go
@@ -22,3 +22,44 @@ func ExampleMarkdown() {
 	// | Body 1 | Body 2 | Body 3 |
 	// | Body 4 | Body 5 | Body 6 |
 }
+
+func ExampleHTML() {
+	matrix := [][]string{
+
+		{"Head 1", "Head 2", "Head 3"},
+		{"Body 1", "Body 2", "Body 3"},
+		{"Body 4", "Body 5", "Body 6"},
+	}
+	for _, row := range table.HTML(matrix) {
+		fmt.Println(row)
+	}
+	// Output:
+	// <table>,
+	// <thead>,
+	// <tr><td>Head 1</td><td>Head 2</td><td>Head 3</td></tr>,
+	// </thead>,
+	// <tbody>,
+	// <tr><td>Body 1</td><td>Body 2</td><td>Body 3</td></tr>,
+	// <tr><td>Body 4</td><td>Body 5</td><td>Body 6</td></tr>,
+	// </tbody>,
+	// </table>,
+}
+
+func ExampleAsciidoc() {
+	matrix := [][]string{
+
+		{"Head 1", "Head 2", "Head 3"},
+		{"Body 1", "Body 2", "Body 3"},
+		{"Body 4", "Body 5", "Body 6"},
+	}
+	for _, row := range table.Markdown(matrix) {
+		fmt.Println(row)
+	}
+	// Output:
+	// [options=header],
+	// |=================,
+	// | Head 1 | Head 2 | Head 3,
+	// | Body 1 | Body 2 | Body 3,
+	// | Body 4 | Body 5 | Body 6,
+	// |=================,
+}
